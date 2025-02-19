a58335f554486f962a8cc713de3694556c26828f
pahulgogna <pahulgogna@gmail.com>
2025-02-20 05:26:02 +0530

    Implement PDF download feature and update components for improved functionality

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,14 +1,39 @@
+import { ArrowDownToLine } from 'lucide-react'
 import MarkdownRenderer from '../utils/MarkdownRender'
+import axios from 'axios'
+import { useState } from 'react'
 
 function Note({ title, content }
     : { 
         title: string,
         content: string
     }) {
+
+      const [loading, setLoading] = useState(false)
+
+    async function downloadPDF() {
+
+        setLoading(true)
+        const url = 'http://localhost:3001/convert'
+        
+        const response = (await axios.post(url, {
+          "markdown": content
+        })).data
+
+        window.open(response.pdfUrl, "_blank");
+
+        setLoading(false)
+
+    }
+
   return (
     <>
-      <div className='flex justify-between '>
+
+      <div className='flex justify-between'>
         <h1 className='text-3xl font-bold'>{title}</h1>
+
+        <ArrowDownToLine onClick={downloadPDF} className={` cursor-pointer ${loading ? "animate-bounce" : null}`}/>
+
       </div>
       <div className='border mt-5 p-5 rounded-xl text-left tracking-wider'>
           <MarkdownRenderer markdownText={content}/>
--- frontend/src/pages/Notes.tsx
+++ frontend/src/pages/Notes.tsx
@@ -6,7 +6,10 @@ function Notes() {
   const notes = useRecoilValueLoadable(notesAtom);
 
   if (notes.state === "loading") {
-    return <div className="text-center text-white text-xl">Loading...</div>;
+    return <div className="min-h-screen flex items-center justify-center bg-cover bg-center p-6"
+      style={{ backgroundImage: "url('images/background.png')" }}>
+        Loading...
+        </div>;
   }
 
   return (
@@ -14,16 +17,16 @@ function Notes() {
       className="min-h-screen flex items-center justify-center bg-cover bg-center p-6"
       style={{ backgroundImage: "url('images/background.png')" }}
     >
-      <div className="w-full max-w-3xl bg-black/70 backdrop-blur-md p-8 rounded-xl shadow-lg border border-gray-700">
+      <div className="w-full max-w-3xl bg-black/70 backdrop-blur-md p-8 mt-30 rounded-xl shadow-lg border border-gray-700">
         <h2 className="text-3xl font-bold text-[#18cb96] text-center mb-6">Your Notes</h2>
 
         <div className="grid grid-cols-1 gap-4">
-          {notes.contents.map((note: NoteSchema) => (
+          {notes.contents.map((note: NoteSchema, index: Number) => (
             <div
               key={note.id}
               className="bg-black/60 border border-gray-600 p-4 rounded-lg shadow-md transition-transform transform hover:scale-105 hover:shadow-xl"
             >
-              <NotePreview title={note.title} content={note.content} id={note.id} />
+              <NotePreview title={`Note: ${index + 1}`} content={note.content} id={note.id} />
             </div>
           ))}
         </div>
